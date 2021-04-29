8155f488e9719a9cdaf343506cbc7ec3ded71042
Nnanyielugo <kz.onyegbule@gmail.com>
2021-04-29 21:27:00 +0100

    set node env to conditionally enable logging

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import * as logger from 'morgan';
 import { appRouter as apiRouter } from './routes';
-const app: express.Express = express();
 
-app.use(logger('dev'));
+const app: express.Express = express();
+const isDev: boolean = process.env.NODE_ENV === 'development';
+if (isDev) {
+  app.use(logger('dev'));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
