a765cf51134522e7fc0b2cd489990cf30310cd6d
Nnanyielugo <kz.onyegbule@gmail.com>
2021-04-29 21:18:28 +0100

    disable createdAt and updatedAt fields

--- backend/src/models/event.ts
+++ backend/src/models/event.ts
@@ -11,29 +11,37 @@ interface Event extends Model {
   mood: string;
 }
 
-export const EventModel = db.sequelize.define<Event>('events', {
-  id: {
-    allowNull: false,
-    type: DataTypes.STRING,
-    primaryKey: true,
-    unique: true,
+export const EventModel = db.sequelize.define<Event>(
+  'events',
+  {
+    id: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      primaryKey: true,
+      unique: true,
+    },
+    event_type: {
+      type: DataTypes.STRING,
+    },
+    visit_id: {
+      type: DataTypes.STRING,
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+    },
+    caregiver_id: {
+      type: DataTypes.STRING,
+    },
+    care_recipient_id: {
+      type: DataTypes.STRING,
+    },
+    mood: {
+      type: DataTypes.STRING,
+    },
   },
-  event_type: {
-    type: DataTypes.STRING,
-  },
-  visit_id: {
-    type: DataTypes.STRING,
-  },
-  timestamp: {
-    type: DataTypes.DATE,
-  },
-  caregiver_id: {
-    type: DataTypes.STRING,
-  },
-  care_recipient_id: {
-    type: DataTypes.STRING,
-  },
-  mood: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false,
+  }
+);
