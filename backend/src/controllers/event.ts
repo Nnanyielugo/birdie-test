c0e898fdc0756d9d820ff394cfbc4792c025ec5f
Nnanyielugo <kz.onyegbule@gmail.com>
2021-04-29 21:16:01 +0100

    issues with getting the query to work. used the native driver and dicovered fields that caused error

--- backend/src/controllers/event.ts
+++ backend/src/controllers/event.ts
@@ -1,15 +1,50 @@
 import { Request, Response, NextFunction } from 'express';
-import { EventModel } from '../models/event';
+import * as config from 'config';
+import * as mysql from 'mysql2';
+
+interface DbConfigOptions {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  // dialect: Dialect;
+}
 
 export async function findAll(
-  _req: Request,
+  req: Request,
   res: Response,
   _next: NextFunction
 ): Promise<void> {
-  try {
-    const events = await EventModel.findAll();
-    res.status(200).json({
-      events,
+  const dbConfig: DbConfigOptions = config.get('dbConfig');
+  let limit = 50;
+  let skip = 0;
+
+  if (typeof req.query.limit === 'string') {
+    limit = parseInt(req.query.limit);
+  }
+
+  if (typeof req.query.skip === 'string') {
+    skip = parseInt(req.query.skip);
+  }
+
+  const con = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.username,
+    password: dbConfig.password,
+    database: dbConfig.database,
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected To The Database!');
+    // Select Data
+    var sql = `SELECT id, event_type, visit_id, timestamp, caregiver_id, care_recipient_id FROM events LIMIT ${limit} OFFSET ${skip}`;
+    con.query(sql, function(err, results, _fields) {
+      if (err) throw err;
+      res.status(200).json({
+        results,
+      });
     });
-  } catch (err) {}
+  });
 }
