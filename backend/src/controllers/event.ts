c045704fe4787bb406d05dbf8e8f92e1d0557821
Nnanyielugo <kz.onyegbule@gmail.com>
2021-04-29 21:20:41 +0100

    use query to select the payload attribute because the 'mood' column will always cause a timeout, and we cannot take it out

--- backend/src/controllers/event.ts
+++ backend/src/controllers/event.ts
@@ -1,50 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
-import * as config from 'config';
-import * as mysql from 'mysql2';
-
-interface DbConfigOptions {
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-  // dialect: Dialect;
-}
+import { EventModel } from '../models/event';
 
 export async function findAll(
   req: Request,
   res: Response,
-  _next: NextFunction
+  next: NextFunction
 ): Promise<void> {
-  const dbConfig: DbConfigOptions = config.get('dbConfig');
-  let limit = 50;
-  let skip = 0;
+  try {
+    let limit = 50;
+    let skip = 0;
 
-  if (typeof req.query.limit === 'string') {
-    limit = parseInt(req.query.limit);
-  }
+    if (typeof req.query.limit === 'string') {
+      limit = parseInt(req.query.limit);
+    }
 
-  if (typeof req.query.skip === 'string') {
-    skip = parseInt(req.query.skip);
-  }
+    if (typeof req.query.skip === 'string') {
+      skip = parseInt(req.query.skip);
+    }
 
-  const con = mysql.createConnection({
-    host: dbConfig.host,
-    user: dbConfig.username,
-    password: dbConfig.password,
-    database: dbConfig.database,
-  });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected To The Database!');
-    // Select Data
-    var sql = `SELECT id, event_type, visit_id, timestamp, caregiver_id, care_recipient_id FROM events LIMIT ${limit} OFFSET ${skip}`;
-    con.query(sql, function(err, results, _fields) {
-      if (err) throw err;
-      res.status(200).json({
-        results,
-      });
+    const events = await EventModel.findAll({
+      limit: limit,
+      offset: skip,
+      attributes: ['payload'],
+      order: [['timestamp', 'desc']],
     });
-  });
+    res.status(200).json({
+      events,
+    });
+  } catch (err) {
+    next(err);
+  }
 }
